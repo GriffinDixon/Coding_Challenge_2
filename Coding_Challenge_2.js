39a1a1ffc28816778b6674673122415c9c520c63
Griffin Dixon <gtd0604@icloud.com>
2024-09-08 11:23:40 -0400

    Add output functionality to display bill, tip, and total

--- Coding_Challenge_2.js
+++ Coding_Challenge_2.js
@@ -2,3 +2,7 @@
 let bill = 175;
 let tip = bill >= 100 ? bill * 0.25 : bill * 0.18;
 console.log (`Tip: $${tip}`)
+
+// Output Details
+let total = bill + tip;
+console.log(`The bill was $${bill}, the tip was $${tip}, and the total value is $${total}`)
